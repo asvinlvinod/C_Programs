46b37aa631a3f6a08d21d17fea08fdc12b5e3a82
Raj Padmanabh A <61701158+rajavu007@users.noreply.github.com>
2020-10-20 21:18:14 +0530

    Update calc.c

--- calc.c
+++ calc.c
@@ -12,3 +12,19 @@ printf("\nProduct=%d",p);
 printf("\nDivision=%d",D);
 }
 
+------------------------------------------------------
+ #include <stdio.h>
+main()
+{
+int a,b,s=0,d=0,p=0,D=0;
+printf("Enter the values for a and b");
+scanf("a=%d & b=%d",&a,&b);
+s=a+b;
+d=a-b;
+p=a*b;
+D=a/b;
+printf("Sum=%d",s);
+printf("\nDifference=%d",d);
+printf("\nProduct=%d",p);
+printf("\nDivision=%d",D);
+}
